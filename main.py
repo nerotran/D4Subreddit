270811669fc231aafcd5a2ba59e074327edcd841
Nero Tran <tranhuq@miamioh.edu>
2023-06-15 19:43:20 -0400

    Change the chart of others category

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-stat = []
-labels = []
+stat = [0,0,0]
+labels = ['Actual complaints\n', 'Complaints about complaints\n', "Others"]
 
 with open('data') as file:
     for line in file:
     	if line in labels:
     		stat[labels.index(line)] += 1
     	else:
-    		labels.append(line)
-    		stat.append(1)
+    		stat[2] += 1
 
 y = np.array(stat)
 
 plt.pie(y, labels = labels, autopct='%1.1f%%')
-plt.title('Diablo 4 Subreddit Post Types')
+plt.title('Diablo 4 Subreddit Post Types (Out of 200+ top posts)')
 plt.show()
